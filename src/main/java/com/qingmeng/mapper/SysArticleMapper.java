196eb064161faad8c68e5b24230cde8c01dc9328
清梦 <90257473+cyljx9999@users.noreply.github.com>
2023-11-08 15:48:41 +0800

    更新依赖和以及项目工具配置

---
+++ src/main/java/com/qingmeng/mapper/SysArticleMapper.java
@@ -0,0 +1,18 @@
+package com.qingmeng.mapper;
+
+import com.qingmeng.domain.SysArticle;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Administrator
+* @description 针对表【sys_article(物品信息表)】的数据库操作Mapper
+* @createDate 2023-11-08 09:20:04
+* @Entity com.qingmeng.domain.SysArticle
+*/
+public interface SysArticleMapper extends BaseMapper<SysArticle> {
+
+}
+
+
+
+
